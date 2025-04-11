547f1384be4479c24d7097064e1f8e8182d3c854
sandro-sa <sandrosa0315@outlook.com>
2025-04-11 18:56:20 -0300

    Refatoração de tons musicais

--- app/Http/Controllers/Api/App/ChordService/RetornaListaDeNomesEhPosicaoOndeInciaOhAcorde.php
+++ app/Http/Controllers/Api/App/ChordService/RetornaListaDeNomesEhPosicaoOndeInciaOhAcorde.php
@@ -9,19 +9,19 @@ class RetornaListaDeNomesEhPosicaoOndeInciaOhAcorde{
     public static function retornaListaDeNomesEhPosicaoOndeInciaOhAcorde( $posicao_onde_comeca_acorde, $nomeDoAcorde ){
 
         $escalaMaior = [
-            "C"  => 1,  "Db" => 2, "D" => 3,
-            "Eb" =>4 ,  "E"  => 5, "F"  => 6,
-            "F#" => 7 , "G"  => 8, "Ab" => 9 ,
-            "A"  => 10, "Bb" => 11 ,"B" => 12
+            "C"  => 1, "C#" => 2, "Db" => 2, "D" => 3,
+            "D#" =>4 , "Eb" =>4 , "E"  => 5, "F"  => 6,
+            "F#" => 7 ,"Gb" => 7 , "G"  => 8, "G#" => 9 ,"Ab" => 9 ,
+            "A"  => 10, "A#" => 11 ,"Bb" => 11 ,"B" => 12
         ];
     
-        $escalaMenor = [
-            "C" => 1,
-            "C#" => 2, "D" => 3,
-            "Eb" =>4 , "E" => 5, "F" =>6,
-            "F#" => 7 , "G" => 8,
-            "G#" => 9 , "A" => 10, "Bb" => 11 , "B" => 12
+        $escalaMenor =  [
+            "C"  => 1, "C#" => 2, "Db" => 2, "D" => 3,
+            "D#" =>4 , "Eb" =>4 , "E"  => 5, "F"  => 6,
+            "F#" => 7 ,"Gb" => 7 , "G"  => 8, "G#" => 9 ,"Ab" => 9 ,
+            "A"  => 10, "A#" => 11 ,"Bb" => 11 ,"B" => 12
         ];
+        
         $escalaVerificacao = [
             "C","D", "E", "F", "G", "A", "B"
         ];
--- app/Http/Controllers/Web/AppWeb/ShowMusicController.php
+++ app/Http/Controllers/Web/AppWeb/ShowMusicController.php
@@ -11,22 +11,54 @@ class ShowMusicController extends Controller
 {
     private $escalaMaior = [
         "C" => 1,
-        "Db" => 2, "D" => 3,
-        "Eb" =>4 , "E" => 5, "F" =>6,
-        "F#" => 7 , "G" => 8,
-        "Ab" => 9 , "A" => 10, "Bb" => 11 , "B" => 12
+        "C#" => 2, "D" => 3,
+        "D#" => 4, "E" => 5, "F" => 6,
+        "F#" => 7, "G" => 8,
+        "G#" => 9, "A" => 10,
+        "A#" => 11, "B" => 12
     ];
-
+    
     private $escalaMenor = [
         "C" => 1,
         "C#" => 2, "D" => 3,
-        "Eb" =>4 , "E" => 5, "F" =>6,
-        "F#" => 7 , "G" => 8,
-        "G#" => 9 , "A" => 10, "Bb" => 11 , "B" => 12
+        "D#" => 4, "E" => 5, "F" => 6,
+        "F#" => 7, "G" => 8,
+        "G#" => 9, "A" => 10,
+        "A#" => 11, "B" => 12
     ];
 
     private $escalaVerificacao = [
         "C","D", "E", "F", "G", "A", "B"
+    ];
+
+    private $escalaSustenida = [
+        "C" => 1,
+        "C#" => 2,
+        "D" => 3,
+        "D#" => 4,
+        "E" => 5,
+        "F" => 6,
+        "F#" => 7,
+        "G" => 8,
+        "G#" => 9,
+        "A" => 10,
+        "A#" => 11,
+        "B" => 12
+    ];
+    
+    private $escalaBemol = [
+        "C" => 1,
+        "Db" => 2,
+        "D" => 3,
+        "Eb" => 4,
+        "E" => 5,
+        "F" => 6,
+        "Gb" => 7,
+        "G" => 8,
+        "Ab" => 9,
+        "A" => 10,
+        "Bb" => 11,
+        "B" => 12
     ];
 
     public function index($id, $posicaoDoNovoTom = null)
@@ -36,19 +68,13 @@ class ShowMusicController extends Controller
         if(!$music){
             return view('error.not-found');
         }
-
         $tom = $this->retornaTom($music->tone->tone);
         $acordeEhMenor = $this->verificaSeAcordeEhMenor($music->tone->tone);
+        //Aqui entra o tipo da escala (sustenido ou bemol)
+        $tipoEscala = $this->tipoDeEscalaPorTom($music->tone->tone);
+        // Aqui define qual escala usar (maior ou menor)
+        $escala = $acordeEhMenor ? $this->escalaMenor : $this->escalaMaior;
 
-        
-
-        $escala = null;
-        if($acordeEhMenor){
-            $escala = $this->escalaMenor;
-        }else{
-            $escala = $this->escalaMaior;
-
-        }
         foreach ($escala as $chave => $valor) {
             if($tom == $chave){
                 $posicao = $valor;
@@ -56,22 +82,18 @@ class ShowMusicController extends Controller
         }
 
         $listaDeTons = RetornaListaDeNomesEhPosicaoOndeInciaOhAcorde::retornaListaDeNomesEhPosicaoOndeInciaOhAcorde($posicao, $music->tone->tone);
-    
-        
-
         $arrayChords = json_decode($music->chords);
-
         $listaDeAcordeParaMudarTom = false;
 
         if($posicaoDoNovoTom != null){
-            if($posicaoDoNovoTom < $posicao ||$posicaoDoNovoTom > $posicao){
+            if($posicaoDoNovoTom < $posicao || $posicaoDoNovoTom > $posicao){
                 $novaPosicao = $posicao - $posicaoDoNovoTom;
-                $listaDeAcordeParaMudarTom = $this->retornaListaDeAcordeParaMudarTom($arrayChords, $novaPosicao, $acordeEhMenor);
-                $arrayChords = $this->retornaNovosAcordes($arrayChords, $novaPosicao, $acordeEhMenor);
+                $listaDeAcordeParaMudarTom = $this->retornaListaDeAcordeParaMudarTom($arrayChords, $novaPosicao, $acordeEhMenor, $tipoEscala);
+                
+                $arrayChords = $this->retornaNovosAcordes($arrayChords, $novaPosicao, $acordeEhMenor, $tipoEscala);
             }
         }
 
-       
         $chords = Chord::whereIn('chord_name', $arrayChords)->get();
 
         $tons = [];
@@ -84,12 +106,12 @@ class ShowMusicController extends Controller
         
         $novoTom = false;
         if($listaDeAcordeParaMudarTom){
-           $novoTom = $this->retornaNovoTom($music->tone->tone,$listaDeAcordeParaMudarTom, $posicaoDoNovoTom);
+           $novoTom = $this->retornaNovoTom($music->tone->tone,$listaDeAcordeParaMudarTom, $posicaoDoNovoTom );
         }
        
        $listaDeTons = json_encode($tons);
        $listaDeAcordeParaMudarTom = json_encode($listaDeAcordeParaMudarTom);
-        
+     //  dd($listaDeAcordeParaMudarTom);
         $music->load(['tone','singer','rhythm']);
         return view('web.show-music',[
             'music' => $music,
@@ -100,36 +122,96 @@ class ShowMusicController extends Controller
         ]);
       
     }
-
     private function retornaTom($tomOriginal){
-      
-        $tom = substr($tomOriginal, 0, 1);
+        $tom = strtoupper(substr($tomOriginal, 0, 1));
         $segundoCaracterDoAcorde = mb_substr($tomOriginal, 1, 1, 'UTF-8');
-        if($segundoCaracterDoAcorde == "b" || $segundoCaracterDoAcorde == "#"){
-            $tom = $tom.$segundoCaracterDoAcorde;
+    
+        if ($segundoCaracterDoAcorde == "b" || $segundoCaracterDoAcorde == "#") {
+            $tom .= $segundoCaracterDoAcorde;
         }
-
+    
         return $tom;
     }
-
     private function verificaSeAcordeEhMenor($nomeDoAcorde){
 
         $segundoCaracter = mb_substr($nomeDoAcorde, 1, 1, 'UTF-8');
         $terceiroCaractere = mb_substr($nomeDoAcorde, 2, 1, 'UTF-8');
-        
         if($segundoCaracter == "m"|| $terceiroCaractere == "m"){
             return true;
         }
-
         return false;
 
     }
+    private function retornaNovaNota($novaPosicao, $nota, $tipoEscala)
+    {
+        $escala = $this->obterEscalaCorreta($tipoEscala);
+    
+        // 🧪 SE NÃO EXISTIR NA ESCALA, TENTA TROCAR A ESCALA!
+        if (!isset($escala[$nota])) {
+            // Tenta outra escala
+            $tipoEscalaAlternativa = $tipoEscala === 'bemol' ? 'sustenido' : 'bemol';
+            $escala = $this->obterEscalaCorreta($tipoEscalaAlternativa);
+        }
+    
+        // Ainda não encontrou? Aborta e retorna a nota original
+        if (!isset($escala[$nota])) {
+            return $nota;
+        }
+    
+        $posicaoNotaOriginal = $escala[$nota];
+    
+        // Faz o deslocamento circular corretamente (1-12)
+        $novaPosicaoNota = (($posicaoNotaOriginal - $novaPosicao - 1 + 12) % 12) + 1;
+    
+        // Busca o nome da nova nota
+        foreach ($escala as $nomeNota => $valor) {
+            if ($valor === $novaPosicaoNota) {
+                return $nomeNota;
+            }
+        }
+    
+        return $nota; // fallback de segurança
+    }
+    
+    private function retornaListaDeAcordeParaMudarTom($arrayChords, $novaPosicao, $escalaEhMenor,$tipoEscala){
+        
+        $cont = 0;
+        foreach($arrayChords as $key => $value){
 
+            $nota = $this->retornaTom($value);
+            
+            $novaNota = $this->retornaNovaNota($novaPosicao, $nota,$tipoEscala);
+            
+            $restanteDoAcorde = $this->retornaRestanteDoAcorde($value,$novaPosicao,$escalaEhMenor);
+            
+            $novosAcordes[$value] = $novaNota.$restanteDoAcorde;
+            $cont++;
+        }
+
+        return $novosAcordes;
+    }
+    private function retornaNovosAcordes($arrayChords, $novaPosicao,  $escalaEhMenor,$tipoEscala){
+
+        foreach($arrayChords as $key => $value){
+            
+            $nota = $this->retornaTom($value);
+
+            $novaNota = $this->retornaNovaNota($novaPosicao, $nota,$tipoEscala );
+            
+
+            $restanteDoAcorde = $this->retornaRestanteDoAcorde($value,$novaPosicao, $escalaEhMenor);
+
+            $novosAcordes[ $key] = $novaNota.$restanteDoAcorde;
+        }
+
+        return $novosAcordes;
+    }
     private function retornaRestanteDoAcorde($tom, $novaPosicao, $escalaEhMenor){
-      
+    
         $restanteDoAcorde = substr($tom, 1);
         $sustenidoOuBemol = mb_substr($tom, 1, 1, 'UTF-8');
-        if($sustenidoOuBemol == "b" || $tom == "#"){
+       
+        if($sustenidoOuBemol == "b" || $sustenidoOuBemol == "#"){
             $restanteDoAcorde = substr($tom, 2);
         }
 
@@ -142,73 +224,6 @@ class ShowMusicController extends Controller
         return $restanteDoAcorde;
 
     }
-
-    private function retornaNovaNota($novaPosicao, $nota, $acordeEhMenor){
-
-        $posicao = null;
-        $escala = [];
-        if($acordeEhMenor){
-            $escala = $this->escalaMenor;
-        }else{
-            $escala = $this->escalaMaior;
-        }
-
-        foreach ($escala as $chave => $valor) {
-            if($nota == $chave){
-                $posicao = $valor - $novaPosicao;
-            }
-        }
-
-        if(!$posicao){
-            $posicao = 12;
-         }
-         else if($posicao < 0){
-             $posicao = 12 - ($posicao *-1);
-         }
-         else if($posicao > 12){
-             $posicao = $posicao - 12;
-         }
-         
-         foreach ($escala as $chave => $valor) {
-             if($posicao == $valor){
-                 return $chave;
-             }
-         }
-
-    }
-    
-    private function retornaListaDeAcordeParaMudarTom($arrayChords, $novaPosicao, $escalaEhMenor){
-        
-        foreach($arrayChords as $key => $value){
-
-            $nota = $this->retornaTom($value);
-
-            $novaNota = $this->retornaNovaNota($novaPosicao, $nota, $escalaEhMenor,$escalaEhMenor);
-
-            $restanteDoAcorde = $this->retornaRestanteDoAcorde($value,$novaPosicao,$escalaEhMenor);
-    
-            $novosAcordes[$value] = $novaNota.$restanteDoAcorde;
-        }
-
-        return $novosAcordes;
-    }
-
-    private function retornaNovosAcordes($arrayChords, $novaPosicao,  $escalaEhMenor){
-
-        foreach($arrayChords as $key => $value){
-            
-            $nota = $this->retornaTom($value);
-
-            $novaNota = $this->retornaNovaNota($novaPosicao, $nota, $escalaEhMenor );
-
-            $restanteDoAcorde = $this->retornaRestanteDoAcorde($value,$novaPosicao, $escalaEhMenor);
-
-            $novosAcordes[ $key] = $novaNota.$restanteDoAcorde;
-        }
-
-        return $novosAcordes;
-    }
-
     private function retornaNovoTom($tom, $arrayDeTons, $novaPosicao){
 
         foreach($arrayDeTons as $key => $value){
@@ -235,8 +250,7 @@ class ShowMusicController extends Controller
     
         }
     }
-
-    private function verificaSeAcordeTemTomMaior($restanteDoAcorde,$novaPosicao){
+    private function verificaSeAcordeTemTomMaior($restanteDoAcorde){
         
         $contémLetras = false;
 
@@ -288,5 +302,28 @@ class ShowMusicController extends Controller
             }
         }
 
+    }
+    private function tipoDeEscalaPorTom($tomOriginal)
+    {
+        $tomBase = strtoupper($this->retornaTom($tomOriginal));
+
+        if (str_contains($tomBase, 'B')) {
+            return 'bemol';
+        }
+
+        $tonsComBemol = ['F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'Cb'];
+        if (in_array($tomBase, $tonsComBemol)) {
+            return 'bemol';
+        }
+
+        return 'sustenido';
+    }
+    private function obterEscalaCorreta( $tipoEscala)
+    {
+        if ($tipoEscala === 'bemol') {
+            return $this->escalaBemol;
+        }
+
+        return $this->escalaSustenida;
     }
 }
