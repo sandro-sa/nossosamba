b80226784be8b385faed3b4e5ef9b076b68fd9ca
sandro-sa <sandrosa0315@outlook.com>
2025-04-18 10:09:20 -0300

    Teste mostrar acorde

--- app/Http/Controllers/Web/AppWeb/ShowMusicController.php
+++ app/Http/Controllers/Web/AppWeb/ShowMusicController.php
@@ -9,23 +9,37 @@ use App\Http\Controllers\Api\App\ChordService\RetornaListaDeNomesEhPosicaoOndeIn
 
 class ShowMusicController extends Controller
 {
+    // private $escalaMaior = [
+    //     "C" => 1,
+    //     "C#" => 2, "D" => 3,
+    //     "D#" => 4, "E" => 5, "F" => 6,
+    //     "F#" => 7, "G" => 8,
+    //     "G#" => 9, "A" => 10,
+    //     "A#" => 11, "B" => 12
+    // ];
+    
+    // private $escalaMenor = [
+    //     "C" => 1,
+    //     "C#" => 2, "D" => 3,
+    //     "D#" => 4, "E" => 5, "F" => 6,
+    //     "F#" => 7, "G" => 8,
+    //     "G#" => 9, "A" => 10,
+    //     "A#" => 11, "B" => 12
+    // ];
     private $escalaMaior = [
-        "C" => 1,
-        "C#" => 2, "D" => 3,
-        "D#" => 4, "E" => 5, "F" => 6,
-        "F#" => 7, "G" => 8,
-        "G#" => 9, "A" => 10,
-        "A#" => 11, "B" => 12
+        "C"  => 1, "C#" => 2, "Db" => 2, "D" => 3,
+        "D#" =>4 , "Eb" =>4 , "E"  => 5, "F"  => 6,
+        "F#" => 7 ,"Gb" => 7 , "G"  => 8, "G#" => 9 ,"Ab" => 9 ,
+        "A"  => 10, "A#" => 11 ,"Bb" => 11 ,"B" => 12
+    ];
+
+    private $escalaMenor =  [
+        "C"  => 1, "C#" => 2, "Db" => 2, "D" => 3,
+        "D#" =>4 , "Eb" =>4 , "E"  => 5, "F"  => 6,
+        "F#" => 7 ,"Gb" => 7 , "G"  => 8, "G#" => 9 ,"Ab" => 9 ,
+        "A"  => 10, "A#" => 11 ,"Bb" => 11 ,"B" => 12
     ];
     
-    private $escalaMenor = [
-        "C" => 1,
-        "C#" => 2, "D" => 3,
-        "D#" => 4, "E" => 5, "F" => 6,
-        "F#" => 7, "G" => 8,
-        "G#" => 9, "A" => 10,
-        "A#" => 11, "B" => 12
-    ];
 
     private $escalaVerificacao = [
         "C","D", "E", "F", "G", "A", "B"
